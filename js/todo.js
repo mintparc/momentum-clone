211520ccffb93749cd7c2c97b5c7db961ae7295a
mint <mintparc@icloud.com>
2021-07-27 19:13:22 +0900

    ✨ Add Todo List Feature
    - Todo list save to LocalStorage
    - Delete Todo list from LocalStorage

---
+++ js/todo.js
@@ -0,0 +1,52 @@
+const toDoForm = document.querySelector('#todo-form')
+const toDoInput = document.querySelector('#todo-form input')
+const toDoList = document.querySelector('#todo-list')
+
+const TODOS_KEY = 'todos'
+let toDos = []
+
+const saveToDos = () => {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
+}
+
+const deleteTodo = (event) => {
+  const li = event.target.parentElement
+  li.remove()
+  toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id))
+  saveToDos()
+}
+
+const paintToDo = (newTodoObj) => {
+  const li = document.createElement('li')
+  li.id = newTodoObj.id
+  const span = document.createElement('span')
+  span.innerText = newTodoObj.text
+  const button = document.createElement('button')
+  button.innerText = '❌'
+  button.addEventListener('click', deleteTodo)
+  li.appendChild(span)
+  li.appendChild(button)
+  toDoList.appendChild(li)
+}
+
+const handleToDoSubmit = (event) => {
+  event.preventDefault()
+  const newTodo = toDoInput.value
+  toDoInput.value = ''
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now()
+  }
+  toDos.push(newTodoObj)
+  paintToDo(newTodoObj)
+  saveToDos()
+}
+
+toDoForm.addEventListener('submit', handleToDoSubmit)
+
+const savedToDos = localStorage.getItem(TODOS_KEY)
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos)
+  toDos = parsedToDos
+  parsedToDos.forEach(paintToDo)
+}
