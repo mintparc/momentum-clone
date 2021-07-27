c73766b22d301caf48c329815f5e519ee173a525
mint <mintparc@icloud.com>
2021-07-27 14:55:56 +0900

    ✨ Add Login Feature
    - Username save to LocalStorage
    - Hidden based on login or not found

---
+++ app.js
@@ -0,0 +1,29 @@
+const loginForm = document.querySelector('#login-form')
+const loginInput = document.querySelector('#login-form input')
+const greeting = document.querySelector('#greeting')
+
+const HIDDEN_CLASSNAME = 'hidden'
+const USERNAME_KEY = 'username'
+
+const onLoginSubmit = (event) => {
+  // preventDefault : 이벤트를 취소할 수 있는 경우, 이벤트의 전파를 막지않고 그 이벤트를 취소합니다.
+  event.preventDefault()
+  const username = loginInput.value
+  loginForm.classList.add(HIDDEN_CLASSNAME)
+  localStorage.setItem(USERNAME_KEY, username)
+  paintGreetings(username)
+}
+
+const paintGreetings = (username) => {
+  greeting.innerText = `Hello ${username}`
+  greeting.classList.remove(HIDDEN_CLASSNAME)
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY)
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME)
+  loginForm.addEventListener('submit', onLoginSubmit)
+} else {
+  paintGreetings(savedUsername)
+}
